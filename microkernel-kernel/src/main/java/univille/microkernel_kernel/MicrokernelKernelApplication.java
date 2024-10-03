b9dc5afb5673d7974fe8c61a945231db00a73322
VieiraLucasDuarte <lucasduartevieira0@gmail.com>
2024-10-03 19:50:15 -0300

    Second Commit

--- microkernel-kernel/src/main/java/univille/microkernel_kernel/MicrokernelKernelApplication.java
+++ microkernel-kernel/src/main/java/univille/microkernel_kernel/MicrokernelKernelApplication.java
@@ -2,8 +2,11 @@ package univille.microkernel_kernel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@ComponentScan(basePackages = {"br.univille"})
+@SpringBootApplication()
 public class MicrokernelKernelApplication {
 
 	public static void main(String[] args) {
